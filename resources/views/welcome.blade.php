d15de8586ab8d6c674267d6c5f845d7ea40c1b8a
valeria <valeria.granada@evertecinc.com>
2021-12-28 09:44:25 -0500

    refactor: styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
     <div class="bg-white rounded-lg shadow-sm p-4 text-center flex flex-col gap-5">
         <h1 class="text-3xl"> @lang('pagination.welcome') </h1>
         <div class="flex justify-center gap-5">
-            <button class="bg-pink-500 px-10 py-2 rounded-full text-white shadow-lg hover:scale-105 ease-in-out duration-700">@lang('posts.titles.create')</button>
-            <button class="bg-white border border-gray-500 px-10 py-2 rounded-full shadow-lg hover:scale-105 ease-in-out duration-700">@lang('posts.titles.index')</button>
+            <button class="btn-primary">@lang('posts.titles.create')</button>
+            <button class="btn-secondary">@lang('posts.titles.index')</button>
         </div>
     </div>
 @endsection
